426ef9bdf7464c91be2236b5df874279c13627cb
Eduardo Vedes <eduardo.vedes@gmail.com>
2021-07-23 15:05:18 +0100

    fixed magit usage

--- osx/emacs/.emacs.d/init.el
+++ osx/emacs/.emacs.d/init.el
@@ -128,7 +128,6 @@
   :config (counsel-projectile-mode))
 
 (use-package magit
-  :commands (magit-status magit-get-current-branch)
   :custom
   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
 
