9684203d5d03538274b378a6b7681b8c723caadb
Eduardo Vedes <eduardo.vedes@gmail.com>
2021-07-25 19:32:01 +0100

    add config for emacs backup files

--- osx/emacs/.emacs.d/init.el
+++ osx/emacs/.emacs.d/init.el
@@ -274,3 +274,20 @@
                    (float-time
                    (time-subtract after-init-time before-init-time)))
            gcs-done))
+
+;; Manage Emacs Backup Files
+
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+(message "Deleting old backup files...")
+(let ((week (* 60 60 24 7))
+      (current (float-time (current-time))))
+  (dolist (file (directory-files temporary-file-directory t))
+    (when (and (backup-file-name-p file)
+               (> (- current (float-time (fifth (file-attributes file))))
+                  week))
+      (message "%s" file)
+      (delete-file file))))
