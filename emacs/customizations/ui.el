13ef14256289d405d2418cbd7dbe241e827c2d07
Eduardo Vedes <eduardo.vedes@gmail.com>
2022-03-16 19:23:34 +0000

    add magit and remove emacs-notes

--- emacs/customizations/ui.el
+++ emacs/customizations/ui.el
@@ -57,6 +57,18 @@
   :init (doom-modeline-mode 1)
   :custom ((doom-modeline-height 15)))
 
+(use-package magit
+  :commands magit-status
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+;; NOTE: Make sure to configure a GitHub token before using this package!
+;; - https://magit.vc/manual/forge/Token-Creation.html#Token-Creation
+;; - https://magit.vc/manual/ghub/Getting-Started.html#Getting-Started
+(use-package forge
+  :after magit)
+
+
 (use-package beacon
  :config
  (progn
