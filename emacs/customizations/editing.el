93a38f5bcc87d0a372fa89f1935a3012a6f9b461
Eduardo Vedes <eduardo.vedes@gmail.com>
2022-03-16 19:51:11 +0000

    updated editing

--- emacs/customizations/editing.el
+++ emacs/customizations/editing.el
@@ -54,3 +54,18 @@
     (quit nil)))
 
 (setq electric-indent-mode nil)
+
+;; move lines up/down
+(defun move-line (n)
+  "Move the current line up or down by N lines."
+  (interactive "p")
+  (setq col (current-column))
+  (beginning-of-line) (setq start (point))
+  (end-of-line) (forward-char) (setq end (point))
+  (let ((line-text (delete-and-extract-region start end)))
+    (forward-line n)
+    (insert line-text)
+    ;; restore point to original column in moved line
+    (forward-line -1)
+    (forward-char col)))
+
