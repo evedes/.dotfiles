325d5c65c30723b2f424b5240b0aa67914bd52ea
Eduardo Vedes <eduardo.vedes@gmail.com>
2022-03-16 02:51:12 +0000

    updated emacs config

--- emacs/customizations/clojure.el
+++ emacs/customizations/clojure.el
@@ -74,6 +74,6 @@
 (eval-after-load 'cider
   '(progn
      (define-key clojure-mode-map (kbd "C-c C-v") 'cider-start-http-server)
-     (define-key clojure-mode-map (kbd "C-M-r") 'cider-refresh)
-     (define-key clojure-mode-map (kbd "C-c u") 'cider-user-ns)
-     (define-key cider-mode-map (kbd "C-c u") 'cider-user-ns)))
+     (define-key clojure-mode-map (kbd "C-M-r")   'cider-refresh)
+     (define-key clojure-mode-map (kbd "C-c u")   'cider-user-ns)
+     (define-key cider-mode-map   (kbd "C-c u")   'cider-user-ns)))
