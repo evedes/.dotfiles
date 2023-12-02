316e7528ed4f5430cab58be8cf7146e8089005ad
Eduardo Vedes <eduardo.vedes@gmail.com>
2023-12-02 13:33:08 +0000

    chore: update nvim config

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,38 +1,38 @@
 return {
-	"zbirenbaum/copilot.lua",
-	cmd = "Copilot",
-	event = "InsertEnter",
-	dependencies = "zbirenbaum/copilot-cmp",
-	config = function()
-		require("copilot").setup(require("copilot").setup({
-			panel = {
-				enabled = true,
-				auto_refresh = false,
-				keymap = {
-					jump_prev = "[[",
-					jump_next = "]]",
-					accept = "<CR>",
-					refresh = "gr",
-					open = "<M-CR>",
-				},
-				layout = {
-					position = "bottom", -- | top | left | right
-					ratio = 0.4,
-				},
-			},
-			suggestion = {
-				enabled = true,
-				auto_trigger = true,
-				debounce = 75,
-				keymap = {
-					accept = "<M-l>",
-					accept_word = false,
-					accept_line = false,
-					next = "<M-]>",
-					prev = "<M-[>",
-					dismiss = "<C-]>",
-				},
-			},
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  dependencies = "zbirenbaum/copilot-cmp",
+  config = function()
+    require("copilot").setup(require("copilot").setup({
+      panel = {
+        enabled = true,
+        auto_refresh = false,
+        keymap = {
+          jump_prev = "[[",
+          jump_next = "]]",
+          accept = "<CR>",
+          refresh = "gr",
+          open = "<M-CR>",
+        },
+        layout = {
+          position = "bottom", -- | top | left | right
+          ratio = 0.4,
+        },
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        debounce = 75,
+        keymap = {
+          accept = "<M-l>",
+          accept_word = false,
+          accept_line = false,
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-]>",
+        },
+      },
       filetypes = {
         yaml = false,
         markdown = false,
@@ -46,11 +46,11 @@ return {
       },
       copilot_node_command = "node", -- Node.js version must be > 16.x
       server_opts_overrides = {},
-		}))
-		require("copilot_cmp").setup({
-			formatters = {
-				insert_text = require("copilot_cmp.format").remove_existing,
-			},
-		})
-	end,
+    }))
+    require("copilot_cmp").setup({
+      formatters = {
+        insert_text = require("copilot_cmp.format").remove_existing,
+      },
+    })
+  end,
 }
