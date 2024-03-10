dbac5a340388990ee9b26d3a19ccb4fe4c2e419f
Eduardo Vedes <eduardo.vedes@gmail.com>
2024-03-10 02:58:22 +0000

    chore: update arch catppuccin

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,21 +3,10 @@ local opts = { noremap = true, silent = true }
 
 keymap.set("n", "x", '"_x"')
 
--- Increment/decrement
-keymap.set("n", "+", "<C-a>")
-keymap.set("n", "-", "<C-x>")
-
--- Select all
-keymap.set("n", "<C-a>", "gg<S-v>G")
-
 -- Save file and quit
 keymap.set("n", "<Leader>w", ":update<Return>", opts)
 keymap.set("n", "<Leader>q", ":quit<Return>", opts)
 keymap.set("n", "<Leader>Q", ":qa<Return>", opts)
---
--- File explorer with NvimTree
-keymap.set("n", "<Leader>f", ":NvimTreeFindFile<Return>", opts)
-keymap.set("n", "<Leader>t", ":NvimTreeToggle<Return>", opts)
 
 -- Tabs
 keymap.set("n", "te", ":tabedit")
@@ -30,7 +19,7 @@ keymap.set("n", "ss", ":split<Return>", opts)
 keymap.set("n", "sv", ":vsplit<Return>", opts)
 
 -- Resize window
-keymap.set("n", "<C-S-h>", "<C-w><")
-keymap.set("n", "<C-S-l>", "<C-w>>")
-keymap.set("n", "<C-S-k>", "<C-w>+")
-keymap.set("n", "<C-S-j>", "<C-w>-")
+keymap.set("n", "<C-A-h>", "<C-w><")
+keymap.set("n", "<C-A-l>", "<C-w>>")
+keymap.set("n", "<C-A-k>", "<C-w>+")
+keymap.set("n", "<C-A-j>", "<C-w>-")
