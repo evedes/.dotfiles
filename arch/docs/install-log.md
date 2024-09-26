d5fd18f209aa0d1146f679c6dda69beb3f67f473
Eduardo Vedes <eduardo.vedes@gmail.com>
2024-09-26 01:24:57 +0100

    chore: update arch config

--- arch/docs/install-log.md
+++ arch/docs/install-log.md
@@ -191,7 +191,7 @@ Save the file, quit, and update grub with
 
 Install:
 
-```
+```zsh
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
--- arch/nvim/lua/config/lazy.lua
+++ arch/nvim/lua/config/lazy.lua
@@ -26,6 +26,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
